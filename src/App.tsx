c1e91556b75db4e5dbcda47e7816b4786ee432fa
YassinL <yassin@botify.com>
2022-10-19 14:23:42 +0200

    feat: first commit setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import "./App.css";
-
 import React from "react";
-import logo from "./logo-pass-culture.svg";
+import { GlobalStyle } from "./styles";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <>
+      <div>Hello</div>
+      <GlobalStyle />
+    </>
   );
 }
 
---
+++ src/styles/base.ts
@@ -0,0 +1,6 @@
+export const base = `
+  body {
+    font-size: 0.9rem;
+    font-family: Helvetica, sans-serif;
+  }
+  `;
